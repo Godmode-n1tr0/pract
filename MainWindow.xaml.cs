f02ee3d820f198295340ae3304068b47c1ea3c6b
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-12-12 10:32:06 +0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace pract7
 {
@@ -14,38 +13,18 @@ namespace pract7
     {
         Random rnd = new();
 
-        private ObservableCollection<string> vrachFiles = new ObservableCollection<string>();
-        private ObservableCollection<string> pacientFiles = new ObservableCollection<string>();
-
+        private List<string> vrachIds = new List<string>();
+        private List<string> pacientIds = new List<string>();
+        
         private Pacient currentPacient = new Pacient();
-        private string selectedVrachFile = "";
+        private string selectedVrachId = "";
         private string vrachPassword = "";
         private string fioText = "";
         private string specText = "";
-        private string selectedPacientFile = "";
-
+        private string selectedPacientId = "";
+        
         private int _vrachCount = 0;
         private int _pacientCount = 0;
-
-        public ObservableCollection<string> VrachFiles
-        {
-            get => vrachFiles;
-            set
-            {
-                vrachFiles = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public ObservableCollection<string> PacientFiles
-        {
-            get => pacientFiles;
-            set
-            {
-                pacientFiles = value;
-                OnPropertyChanged();
-            }
-        }
 
         public Pacient CurrentPacient
         {
@@ -57,12 +36,12 @@ namespace pract7
             }
         }
 
-        public string SelectedVrachFile
+        public string SelectedVrachId
         {
-            get => selectedVrachFile;
+            get => selectedVrachId;
             set
             {
-                selectedVrachFile = value;
+                selectedVrachId = value;
                 OnPropertyChanged();
                 CheckVrach();
             }
@@ -99,12 +78,12 @@ namespace pract7
             }
         }
 
-        public string SelectedPacientFile
+        public string SelectedPacientId
         {
-            get => selectedPacientFile;
+            get => selectedPacientId;
             set
             {
-                selectedPacientFile = value;
+                selectedPacientId = value;
                 OnPropertyChanged();
                 LoadSelectedPacient();
             }
@@ -136,43 +115,52 @@ namespace pract7
             DataContext = this;
             LoadJsonFileNames();
         }
-
+        
         private void LoadJsonFileNames()
         {
             string folder = Directory.GetCurrentDirectory();
             string[] vjsonFiles = Directory.GetFiles(folder, "D_*.json");
             string[] pjsonFiles = Directory.GetFiles(folder, "P_*.json");
-
-            VrachFiles.Clear();
-            PacientFiles.Clear();
-
+            
+            vrachIds.Clear();
+            id_vrach.Items.Clear();
+            
+            pacientIds.Clear();
+            id_pacient.Items.Clear();
+            
             foreach (string filePath in vjsonFiles)
             {
-                VrachFiles.Add(Path.GetFileName(filePath));
+                string fileName = Path.GetFileName(filePath);
+                string id = fileName.Replace("D_", "").Replace(".json", "");
+                vrachIds.Add(id);
+                id_vrach.Items.Add(id);
             }
-
+            
             foreach (string filePath in pjsonFiles)
             {
-                PacientFiles.Add(Path.GetFileName(filePath));
+                string fileName = Path.GetFileName(filePath);
+                string id = fileName.Replace("P_", "").Replace(".json", "");
+                pacientIds.Add(id);
+                id_pacient.Items.Add(id);
             }
-
+            
             VrachCount = vjsonFiles.Length;
             PacientCount = pjsonFiles.Length;
         }
 
         private void CheckVrach()
         {
-            if (!string.IsNullOrEmpty(SelectedVrachFile) && !string.IsNullOrEmpty(VrachPassword))
+            if (!string.IsNullOrEmpty(SelectedVrachId) && !string.IsNullOrEmpty(VrachPassword))
             {
-                string filePath = Path.Combine(Directory.GetCurrentDirectory(), SelectedVrachFile);
-
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), $"D_{SelectedVrachId}.json");
+                
                 if (File.Exists(filePath))
                 {
                     try
                     {
                         string json = File.ReadAllText(filePath);
                         Vrach v = JsonSerializer.Deserialize<Vrach>(json);
-
+                        
                         if (v != null && v.Password == VrachPassword)
                         {
                             FioText = $"{v.Lastname} {v.Name} {v.MiddleName}";
@@ -200,14 +188,16 @@ namespace pract7
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(f.Text) && !string.IsNullOrEmpty(i.Text) && !string.IsNullOrEmpty(o.Text) &&
+            if (!string.IsNullOrEmpty(f.Text) && !string.IsNullOrEmpty(i.Text) && !string.IsNullOrEmpty(o.Text) && 
                 !string.IsNullOrEmpty(par.Text) && !string.IsNullOrEmpty(ppar.Text) && spec.SelectedIndex != -1)
             {
                 bool areEqual = string.Equals(par.Text, ppar.Text, StringComparison.OrdinalIgnoreCase);
                 if (areEqual == true)
                 {
+                    int vrachId = rnd.Next(10000, 99999);
                     var vrach = new Vrach()
                     {
+                        Id = vrachId,
                         Name = f.Text,
                         Lastname = i.Text,
                         MiddleName = o.Text,
@@ -215,35 +205,39 @@ namespace pract7
                         Password = par.Text,
                     };
                     string jsonstring = JsonSerializer.Serialize(vrach);
-                    string a = "D_" + Convert.ToString(rnd.Next(10000, 99999));
-                    string filepath = a + ".json";
-                    File.WriteAllText(filepath, jsonstring);
-
+                    string filepath = $"D_{vrachId}.json";
+                    
+                    using (StreamWriter writer = new StreamWriter(filepath))
+                    {
+                        writer.WriteAsync(jsonstring);
+                    }
+                    
                     f.Text = "";
                     i.Text = "";
                     o.Text = "";
                     par.Text = "";
                     ppar.Text = "";
                     spec.SelectedIndex = -1;
-
+                    
                     LoadJsonFileNames();
                 }
                 else
                 {
                     MessageBox.Show("Пароли не совпадают");
                 }
-            }
+            }    
             else
             {
-                MessageBox.Show("Поля не заполненны полностью, пожалуйста заполните до конца!!!");
+                MessageBox.Show("Поля не заполнены полностью, пожалуйста заполните до конца!!!");
             }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(f_pac.Text) && !string.IsNullOrEmpty(i_pac.Text) &&
+            if (!string.IsNullOrEmpty(f_pac.Text) && !string.IsNullOrEmpty(i_pac.Text) && 
                 !string.IsNullOrEmpty(o_pac.Text) && !string.IsNullOrEmpty(date_pac.Text))
             {
+                int pacientId = rnd.Next(1000000, 9999999);
                 var pacient = new Pacient()
                 {
                     Name = f_pac.Text,
@@ -255,27 +249,30 @@ namespace pract7
                     Diagnosis = "",
                     Recomendations = "",
                 };
-
+                
                 string jsonstring = JsonSerializer.Serialize(pacient, new JsonSerializerOptions
                 {
                     DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
                     WriteIndented = true
                 });
-
-                string a = "P_" + Convert.ToString(rnd.Next(1000000, 9999999));
-                string filepath = a + ".json";
-                File.WriteAllText(filepath, jsonstring);
-
+                
+                string filepath = $"P_{pacientId}.json";
+                
+                using (StreamWriter writer = new StreamWriter(filepath))
+                {
+                    writer.WriteAsync(jsonstring);
+                }
+                
                 f_pac.Text = "";
                 i_pac.Text = "";
                 o_pac.Text = "";
                 date_pac.Text = "";
-
+                
                 LoadJsonFileNames();
             }
             else
             {
-                MessageBox.Show("Поля не заполненны полностью, пожалуйста заполните до конца!!!");
+                MessageBox.Show("Поля не заполнены полностью, пожалуйста заполните до конца!!!");
             }
         }
 
@@ -286,18 +283,29 @@ namespace pract7
 
         private void SavePacient()
         {
-            if (!string.IsNullOrEmpty(SelectedPacientFile))
+            if (!string.IsNullOrEmpty(SelectedPacientId))
             {
                 try
                 {
+                    if (!string.IsNullOrEmpty(SelectedVrachId))
+                    {
+                        CurrentPacient.LastDoctor = SelectedVrachId;
+                    }
+                    
                     var options = new JsonSerializerOptions
                     {
                         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
                         WriteIndented = true
                     };
-
+                    
                     string json = JsonSerializer.Serialize(CurrentPacient, options);
-                    File.WriteAllText(SelectedPacientFile, json);
+                    string filepath = $"P_{SelectedPacientId}.json";
+                    
+                    using (StreamWriter writer = new StreamWriter(filepath))
+                    {
+                        writer.WriteAsync(json);
+                    }
+                    
                     MessageBox.Show("Сохранено");
                 }
                 catch
@@ -309,10 +317,10 @@ namespace pract7
 
         private void LoadSelectedPacient()
         {
-            if (!string.IsNullOrEmpty(SelectedPacientFile))
+            if (!string.IsNullOrEmpty(SelectedPacientId))
             {
-                string filepath = Path.Combine(Directory.GetCurrentDirectory(), SelectedPacientFile);
-
+                string filepath = $"P_{SelectedPacientId}.json";
+                
                 if (File.Exists(filepath))
                 {
                     try
@@ -331,7 +339,30 @@ namespace pract7
 
         private void ChangePatient_Click(object sender, RoutedEventArgs e)
         {
-            SavePacient();
+            if (!string.IsNullOrEmpty(SelectedPacientId))
+            {
+                try
+                {
+                    string jsonstring = JsonSerializer.Serialize(CurrentPacient, new JsonSerializerOptions
+                    {
+                        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                        WriteIndented = true
+                    });
+                    
+                    string filepath = $"P_{SelectedPacientId}.json";
+                    
+                    using (StreamWriter writer = new StreamWriter(filepath))
+                    {
+                        writer.WriteAsync(jsonstring);
+                    }
+                    
+                    MessageBox.Show("Изменения сохранены");
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка сохранения");
+                }
+            }
         }
 
         private void Reset_Click(object sender, RoutedEventArgs e)
