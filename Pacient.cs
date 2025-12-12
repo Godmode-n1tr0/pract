27ea7d525f36d8c867bd3000c613b58f4f82d44a
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-12-12 10:29:20 +0500

    Update Pacient.cs

--- Pacient.cs
+++ Pacient.cs
@@ -216,4 +216,95 @@ namespace pract7
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
+}        private string _middleName;
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                if (_middleName != value)
+                {
+                    _middleName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _birthday;
+        public string Birthday
+        {
+            get => _birthday;
+            set
+            {
+                if (_birthday != value)
+                {
+                    _birthday = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private DateTime? _lastAppointment;
+        public DateTime? LastAppointment
+        {
+            get => _lastAppointment;
+            set
+            {
+                if (_lastAppointment != value)
+                {
+                    _lastAppointment = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _lastDoctor;
+        public string LastDoctor
+        {
+            get => _lastDoctor;
+            set
+            {
+                if (_lastDoctor != value)
+                {
+                    _lastDoctor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _diagnosis;
+        public string Diagnosis
+        {
+            get => _diagnosis;
+            set
+            {
+                if (_diagnosis != value)
+                {
+                    _diagnosis = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _recomendations;
+        public string Recomendations
+        {
+            get => _recomendations;
+            set
+            {
+                if (_recomendations != value)
+                {
+                    _recomendations = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
 }
